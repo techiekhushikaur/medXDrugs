0a0e7252f0dead9f90ad596149a3381bb9428b01
techiekhushikaur <131657053+techiekhushikaur@users.noreply.github.com>
2023-04-26 01:30:23 +0530

    Update settings.py

--- medXDrugs/opms/settings.py
+++ medXDrugs/opms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'b_jk^-3-9x5@!xfc#ocq8^=9^2ie2-100uxr2hujt))a=&ni9u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
